461e24c0422be287f6f466974ba6632e59e10f75
AAmberFan <filuwan@gmail.com>
2018-04-09 23:16:47 -0400

    passed

--- main/main.js
+++ main/main.js
@@ -1,6 +1,37 @@
 // Write your cade below:
-function main() {};
+function main(sysInput,useInput) {
+    let samePosNum = isSamePosition(sysInput,useInput);
+    let diffPosNum = isDiffPosition(sysInput,useInput);
+    //console.log(samePosNum);
+    //console.log(printSummary(samePosNum,diffPosNum));
+    return printSummary(samePosNum,diffPosNum);
 
-module.exports = {
-    main
 }
+function isSamePosition(sysInput,useInput){
+    let aNum = 0;
+    for (let i = 0;i<sysInput.length;i++){
+        if(sysInput[i]===useInput[i]){
+            aNum++;
+        }
+    }
+    return aNum;
+}
+function isDiffPosition(sysInput,useInput){
+    let bNum = 0;
+//may have problem
+
+    for(let i of useInput){
+        if(sysInput.includes(i)){
+            bNum++;
+        }
+    }
+    return bNum;
+}
+function printSummary(samePosNum,diffPosNum){
+    let result = samePosNum.toString()+"A";
+    result += (diffPosNum-samePosNum).toString()+'B';
+    return result;
+}
+// main('1234','');
+
+module.exports = main;
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,29 @@
 const main = require('../main/main');
 
-describe('main()', () => {
+describe('main()', function() {
 
+    it('user input is the same as system input',function () {
+        let sysInput = '1234';
+        let userInput = '1234';
+        let expected = '4A0B';
+        expect(main(sysInput,userInput)).toBe(expected);
+    });
+    it('user input have same eles with system but different position',function () {
+        let sysInput = '1234';
+        let userInput = '4321';
+        let expected = '0A4B';
+        expect(main(sysInput,userInput)).toBe(expected);
+    });
+    it('user input have same eles with system but different position',function () {
+        let sysInput = '1234';
+        let userInput = '1643';
+        let expected = '1A2B';
+        expect(main(sysInput,userInput)).toBe(expected);
+    });
+    it('user input have same eles with system but different position',function () {
+        let sysInput = '1234';
+        let userInput = '3947';
+        let expected = '0A2B';
+        expect(main(sysInput,userInput)).toBe(expected);
+    });
 });
